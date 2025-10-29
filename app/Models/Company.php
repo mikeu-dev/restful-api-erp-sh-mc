f99d623ff7d664176e07ee137c15a1fdf8e46578
Mikeu Dev <rikiruswandi28@gmail.com>
2025-10-29 16:10:43 +0700

    feat:company model

---
+++ app/Models/Company.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Company extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = ['name', 'email', 'phone', 'address'];
+
+    public function settings()
+    {
+        return $this->hasMany(CompanySetting::class);
+    }
+
+    public function users()
+    {
+        return $this->hasMany(User::class);
+    }
+
+    public function employees()
+    {
+        return $this->hasMany(Employee::class);
+    }
+
+    public function projects()
+    {
+        return $this->hasMany(Project::class);
+    }
+
+    public function clients()
+    {
+        return $this->hasMany(Client::class);
+    }
+}
