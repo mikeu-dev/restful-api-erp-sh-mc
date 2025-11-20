b303bd4f1bd311b50e39f051c6131ad26ad1df79
Mikeu Dev <rikiruswandi28@gmail.com>
2025-11-20 23:22:27 +0700

    feat:base service contract

---
+++ app/Contracts/Services/BaseServiceContract.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Contracts\Services;
+
+interface BaseServiceContract
+{
+    public function getAll();
+    public function getById(int $id);
+    public function create(array $data);
+    public function update(int $id, array $data);
+    public function delete(int $id);
+}
