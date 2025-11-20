a9e277bf9ed1b8d31a609dc0688a5616f94cce0e
Mikeu Dev <rikiruswandi28@gmail.com>
2025-11-20 23:26:32 +0700

    feat:module users prefix endpoints

---
+++ routes/modules/user.php
@@ -0,0 +1,12 @@
+<?php
+
+use App\Modules\User\Controller\UserController;
+use Illuminate\Support\Facades\Route;
+
+Route::prefix('users')->group(function () {
+    Route::get('/', [UserController::class, 'index']);
+    Route::get('{id}', [UserController::class, 'show']);
+    Route::post('/', [UserController::class, 'store']);
+    Route::put('{id}', [UserController::class, 'update']);
+    Route::delete('{id}', [UserController::class, 'destroy']);
+});
