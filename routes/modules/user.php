34de5784a0cf2ab109862f1c9d4b3ee9fdf1310a
Mikeu Dev <rikiruswandi28@gmail.com>
2025-11-21 20:28:49 +0700

    fix:users endpoint with middleware

--- routes/modules/user.php
+++ routes/modules/user.php
@@ -3,7 +3,7 @@
 use App\Modules\User\Controller\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('users')->group(function () {
+Route::prefix('users')->middleware('jwt')->group(function () {
     Route::get('/', [UserController::class, 'index']);
     Route::get('{id}', [UserController::class, 'show']);
     Route::post('/', [UserController::class, 'store']);
