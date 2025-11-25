baae62ad4dc5122ce1b66562407344f2ab6d9b68
Mikeu Dev <rikiruswandi28@gmail.com>
2025-11-25 14:28:24 +0700

    feat(model):company setting with relation to company

---
+++ app/CompanySetting.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CompanySetting extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'company_id',
+        'key',
+        'value',
+    ];
+
+    public function company()
+    {
+        return $this->belongsTo(Company::class);
+    }
+}
